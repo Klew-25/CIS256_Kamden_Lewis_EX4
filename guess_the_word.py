ca2cb906cbb87f4f1e47399ad8478130a533f7d7
Kamden Lewis <kam2157625@maricopa.edu>
2025-10-29 09:09:25 -0700

    Added a list of random words and imported the random module.

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,1 +1,7 @@
-print('test')
+import random
+
+
+# List of random words
+word_list = ["lantern", "crisp", "meadow", "fragment", "orbit",
+             "velvet", "anchor", "ripple", "canyon", "ember"]
+
