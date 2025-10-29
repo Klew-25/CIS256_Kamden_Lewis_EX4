377afbb0a01cad86e7f77338107a1fc979699f65
Kamden Lewis <kam2157625@maricopa.edu>
2025-10-29 09:53:38 -0700

    Added game logic and flow to the program

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,7 +1,29 @@
 import random
 
-
 # List of random words
 word_list = ["lantern", "crisp", "meadow", "fragment", "orbit",
              "velvet", "anchor", "ripple", "canyon", "ember"]
 
+random_word = list(random.choice(word_list))
+length = len(random_word)
+guess_list = [" "] * length
+guess_attempts = len(random_word) + 3
+
+while True:
+    print(guess_list)
+    print(f'Number of guesses left: {guess_attempts}')
+    guess = input("Guess a letter: ").lower()
+    guess_attempts -= 1
+
+    for i, letter in enumerate(random_word):
+        if letter == guess:
+            guess_list[i] = letter
+
+    if guess_list == random_word:
+        print("\nYou guessed the word!")
+        print(guess_list)
+        break
+
+    if guess_attempts == 0:
+        print("\nSorry, you ran out of guesses!")
+        break
