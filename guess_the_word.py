dea171c49d7b641a0d9c46da75e4b1cff548cf6d
Kamden Lewis <kam2157625@maricopa.edu>
2025-10-30 09:05:53 -0700

    Added functions that can be used for testing in the test file

--- guess_the_word.py
+++ guess_the_word.py
@@ -4,32 +4,50 @@ import random
 word_list = ["lantern", "crisp", "meadow", "fragment", "orbit",
              "velvet", "anchor", "ripple", "canyon", "ember"]
 
-# Randomly picks a word and turns it into a list
-random_word = list(random.choice(word_list))
-# Creates an empty list that matches the length of the random word
-guess_list = [" "] * len(random_word)
-# Establishes guess attempts based on the random word
-guess_attempts = len(random_word) + 6
+def get_random_word():
+    return random.choice(word_list)
 
-# A loop that runs the game. Will end if user wins or runs out of guesses
-while True:
-    print(guess_list)
-    print(f'Number of guesses left: {guess_attempts}')
-    guess = input("Guess a letter: ").lower()
-    guess_attempts -= 1
+def process_guess(random_word, guess_list, guess):
+    result = False
+    for j, element in enumerate(random_word):
+        if element == guess:
+            guess_list[j] = element
+            result = True
+    return result
 
-    # Loops through the random word list and adds any matches to the guess list
-    for i, letter in enumerate(random_word):
-        if letter == guess:
-            guess_list[i] = letter
+if __name__ == "__main__":
+    # Randomly picks a word and turns it into a list
+    random_word = list(random.choice(word_list))
+    # Creates an empty list that matches the length of the random word
+    guess_list = [" "] * len(random_word)
+    # Establishes guess attempts based on the random word
+    guess_attempts = len(random_word) + 6
 
-    # Displays if you guess the random word
-    if guess_list == random_word:
-        print("\nCongratulations! You guessed the word!")
+    # A loop that runs the game. Will end if user wins or runs out of guesses
+    while True:
         print(guess_list)
-        break
+        print(f'Number of guesses left: {guess_attempts}')
+        guess = input("Guess a letter: ").lower()
+        guess_attempts -= 1
 
-    # Displays if you run out of guess attempts
-    if guess_attempts == 0:
-        print("\nSorry, you ran out of guesses!")
-        break
+        # Loops through the random word list and adds any matches to the guess list
+        for i, letter in enumerate(random_word):
+            if letter == guess:
+                guess_list[i] = letter
+
+        # Displays a message if the guess was in the word or not
+        if guess in random_word:
+            print('\nNice, that letter is in the word!')
+        else:
+            print('\nSorry, that letter is not in the word.')
+
+        # Displays if you guess the random word
+        if guess_list == random_word:
+            print("\nCongratulations! You guessed the word!")
+            print(guess_list)
+            break
+
+        # Displays if you run out of guess attempts
+        if guess_attempts == 0:
+            print("\nSorry, you ran out of guesses!")
+            break
